d80fb704a6147af05a5a7371da5d1d2c0cad3a77
unknown <amar113118sa@gmail.com>
2023-11-20 20:16:25 -0800

    first commit

---
+++ dog/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+
+def nishan(request):
+    return render(request,"nishan.html")
+
+def sanjay(request):
+    return render(request,"sanjay.html")    
+
+# Create your views here.
